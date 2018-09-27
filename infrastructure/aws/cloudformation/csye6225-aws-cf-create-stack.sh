040004fe6f535d5cc15b101b6a7b5d4ec4fbd2f5
AshwathSM <ashwathsmhegade@gmail.com>
2018-09-27 14:52:59 -0400

    changed cloud formation scripts and json

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #shell script to create AWS network infrastructures
 
+stack_name=$1
 
 
-aws cloudformation create-stack --stack-name testStack --template-body file://csye6225-cf-networking.json
+aws cloudformation create-stack --stack-name $stack_name --template-body file://csye6225-cf-networking.json
 
