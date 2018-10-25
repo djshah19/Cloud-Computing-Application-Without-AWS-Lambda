74289eb62caa4582b2c49446682adcef594a0df4
AshwathSM <ashwathsmhegade@gmail.com>
2018-10-25 07:12:02 -0400

    minor shell script change

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -8,6 +8,6 @@ bucketName="csye6225-fall2018-$bucket_name.csye6225.com"
 
 echo deleting stack
 
-aws s3 rb s3://{$bucketName} --force
+aws s3 rb s3://$bucketName --force
 
 aws cloudformation delete-stack --stack-name $stack_name
