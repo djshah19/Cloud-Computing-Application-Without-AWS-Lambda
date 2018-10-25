639ceb9b937974f6ff657a8c8141d749c94e2afb
AshwathSM <ashwathsmhegade@gmail.com>
2018-10-25 07:05:54 -0400

    hard coding changes

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
@@ -2,7 +2,28 @@
 #shell script to delete AWS stack
 stack_name=$1
 
+bucket_name=$2
+
+bucketName="code-deploy.$bucket_name.csye6225.com"
+
+
 echo deleting the stack
+
+
+aws s3 rb s3://$bucketName --force
+
+
+aws iam remove-role-from-instance-profile --instance-profile-name ayyodevre --role-name CodeDeployEC2ServiceRole
+
+aws iam delete-role-policy --role-name CodeDeployEC2ServiceRole --policy-name CodeDeploy-EC2-S3
+
+
+aws iam delete-role --role-name CodeDeployEC2ServiceRole
+
+aws iam detach-role-policy --role-name CodeDeployServiceRole --policy-arn arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole
+
+aws iam delete-role --role-name CodeDeployServiceRole
+
 
 
 
